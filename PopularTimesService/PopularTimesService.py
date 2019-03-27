0c5e46964a9b5edec46f9cd02c3442e8a0ea23a7
Marcus Sinhalage <dushshantha.chandaradasa@gmail.com>
2019-03-27 09:23:34 -0400

    removed unwanted files

--- PopularTimesService/PopularTimesService.py
+++ PopularTimesService/PopularTimesService.py
@@ -24,8 +24,34 @@ def getPopularTimes(place_id):
     except Exception as ex:
         return notify_error(ex, HTTP_ERROR_SERVER)
 
-def getPopularPlaces(api_key, types, p1, p2, n_threads = 10, radius =  3000, all_places = False):
-    return jsonify(populartimes.get(api_key, types, p1, p2, n_threads, n_threads, radius, all_places))
+
+@app.route('/populartimes/search')
+def getPopularPlaces():
+    api_key, types, p1, p2,
+    n_threads = 10
+    radius =  3000
+    all_places = False
+
+    if 'apiKey' not in request.args or request.args['apiKey'] in ("", None):
+        return notify_error("ERR_NO_ARG:  'apiKey' argument required to /populartimes/ API", HTTP_ERROR_CLIENT)
+    api_key = request.args.get('apiKey')
+
+    if 'types' not in request.args or request.args['types'] in ("", None):
+        return notify_error("ERR_NO_ARG:  'types' argument required to /populartimes/ API", HTTP_ERROR_CLIENT)
+    types = request.args.getlist('types')
+
+    if 'p1' not in request.args or request.args['p1'] in ("", None):
+        return notify_error("ERR_NO_ARG:  'p1' argument required to /populartimes/ API", HTTP_ERROR_CLIENT)
+    p1 = request.args.get('p1')
+
+    if 'p2' not in request.args or request.args['p2'] in ("", None):
+        return notify_error("ERR_NO_ARG:  'p2' argument required to /populartimes/ API", HTTP_ERROR_CLIENT)
+    p2 = request.args.get('p2')
+
+    try:
+        return jsonify(populartimes.get(api_key, types, p1, p2, n_threads, radius, all_places))
+    except Exception as ex:
+        return notify_error(ex, HTTP_ERROR_SERVER)
 
 
 if __name__ == '__main__':
